f19ffc8bf3e4db7c232e2d40cb762c9d0e9ca6dc
Anthony McDonald <anthony@withplum.com>
2022-04-04 17:27:55 +0100

    Created Feature C

---
+++ FeatureC.swift
@@ -0,0 +1,8 @@
+//
+//  FeatureC.swift
+//  RemovingFileDemo
+//
+//  Created by Anthony McDonald on 04/04/2022.
+//
+
+import Foundation
---
+++ RemovingFileDemo/TopSecret.swift
@@ -0,0 +1,13 @@
+//
+//  TopSecret.swift
+//  RemovingFileDemo
+//
+//  Created by Anthony McDonald on 04/04/2022.
+//
+
+import Foundation
+
+struct TopSecret {
+    let AppKey = "123"
+    let BackdoorKey = "456"
+}
