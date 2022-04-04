8ab4380cb383f44aac26165ddbe0e35fbbc66eda
Anthony McDonald <anthony@withplum.com>
2022-04-04 17:23:32 +0100

    First commitgit add .

---
+++ RemovingFileDemo/RemovingFileDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemovingFileDemoApp.swift
+//  RemovingFileDemo
+//
+//  Created by Anthony McDonald on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct RemovingFileDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
